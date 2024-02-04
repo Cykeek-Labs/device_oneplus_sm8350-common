f151d8ac2cf1c41bd27f29e5711d18775e88387d
cykeek <cykeek@proton.me>
2024-02-04 10:01:14 +0000

    sm8350-common: Decommonize overlays from common to device specific * move sm8350-common overlays to porsche
    Signed-off-by: cykeek <cykeek@proton.me>

--- common.mk
+++ common.mk
@@ -316,9 +316,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-voltage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
